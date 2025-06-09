923d10c87f767f80cc485eabf29368a90862095a
ikeshav26 <keshavgilhotra4@gmail.com>
2025-06-09 22:48:20 +0530

    Background and route setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Project from './pages/Project'
+import Contact from './pages/Contact'
+import About from './pages/About'
+
 
 const App = () => {
   return (
-    <div className='bg-red-300'>
-      Portfolio keshav gilhotra
+    <div className=''>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/projects' element={<Project/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+      </Routes>
     </div>
   )
 }
