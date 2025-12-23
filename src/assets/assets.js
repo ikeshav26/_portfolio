1ec43205949a4c249b96f372f557ede02d045276
ikeshav26 <keshavgilhotra4@gmail.com>
2025-12-23 19:42:19 +0530

    Fixes for preventing from unrequired caching

---
+++ src/assets/assets.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import DiscordIcon from "../icons/DiscordIcon.jsx";
+import { Globe, Github, Linkedin, Twitter } from 'lucide-react';
+
+export { default } from './assets.jsx';
